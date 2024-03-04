e64774dc4f52dc341ae19de7b339be549f40d681
zlatamaltz <161606387+zlatamaltz@users.noreply.github.com>
2024-03-04 19:30:19 +0000

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,5 @@
+srv-http-te
+-----------------------
+### 
+Http service for Google bard request  
+api lib: use [Bard-API](https://github.com/dsdanielpark/Bard-API)
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="srv-http-te",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        'bardapi==0.1.23a0',
+        'Flask==2.3.2',
+    ],
+
+    author="vow1231a",
+    author_email="",
+    description="bard http server",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/zlatamaltz/srv-http-te",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+     project_urls={
+        'Source': "https://github.com/zlatamaltz/srv-http-te",
+    },
+)
